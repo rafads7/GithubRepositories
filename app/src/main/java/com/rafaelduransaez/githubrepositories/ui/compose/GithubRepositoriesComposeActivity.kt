f5bc0678fc604e1d5c3066de0b7a384c2c8bc758
rafads7 <rafaduransaez@gmail.com>
2024-02-10 22:35:29 +0100

    Material Theme added

--- app/src/main/java/com/rafaelduransaez/githubrepositories/ui/compose/GithubRepositoriesComposeActivity.kt
+++ app/src/main/java/com/rafaelduransaez/githubrepositories/ui/compose/GithubRepositoriesComposeActivity.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import com.rafaelduransaez.githubrepositories.ui.compose.ui.components.AppNavGraph
 import com.rafaelduransaez.githubrepositories.ui.compose.ui.theme.GithubRepositoriesTheme
+
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
