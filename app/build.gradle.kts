bd073062d8aed69872772a3dcc633ec099a530b6
rafads7 <rafaduransaez@gmail.com>
2024-11-06 16:28:46 +0100

    kapt little update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.hiltAndroid)
     alias(libs.plugins.navSafeArgs)
     alias(libs.plugins.ksp)
-    id("kotlin-kapt")
+    alias(libs.plugins.kapt)
 }
 
 android {
@@ -37,12 +37,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
-    }
-
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget="21"
-        }
     }
 
     kotlinOptions {
